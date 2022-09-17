aa3523eeaafa523d85f734a9caf589a0ca410c87
shailesh <shaileshpawarm@gmail.com>
2022-09-17 15:36:04 +0530

    admin add

--- ecommerce/product/admin.py
+++ ecommerce/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from  .models import *
 # Register your models here.
+
+admin.site.register(Prodcut_categories) 
+admin.site.register(Product)
+admin.site.register(ProductInCart)
+admin.site.register(Order) 
+admin.site.register(Cart) 
